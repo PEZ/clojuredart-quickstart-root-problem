8eeff5ac43cc178e93fdebbae430d1925df04f3a
Peter Strömberg <pez@pezius.com>
2022-04-16 22:17:06 +0200

    first commit

---
+++ hello-clojurdart/lib/cljd-out/quickstart/helloworld.dart
@@ -0,0 +1,10 @@
+import "dart:core" as dc;
+import "helloworld.dart" as UpPtchclcoq_helloworld;
+import "../cljd/core.dart" as UpPtchclcoc_core;
+
+// BEGIN main
+dc.dynamic main(){
+return (UpPtchclcoc_core.print.$_invoke$1("hello, world\n", ));
+}
+
+// END main
---
+++ helloworld/lib/cljd-out/quickstart/helloworld.dart
@@ -0,0 +1,10 @@
+import "dart:core" as dc;
+import "helloworld.dart" as UpPtchlcoq_helloworld;
+import "../cljd/core.dart" as UpPtchlcoc_core;
+
+// BEGIN main
+dc.dynamic main(){
+return (UpPtchlcoc_core.print.$_invoke$1("hello, world\n", ));
+}
+
+// END main
